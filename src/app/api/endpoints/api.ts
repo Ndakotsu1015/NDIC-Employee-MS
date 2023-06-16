f80a398cce2a85fe507939997eebea1b20d6a3dd
Ndakotsu1015 <maliyu1015@gmail.com>
2023-06-16 22:14:01 +0100

    my feature-employee

---
+++ src/app/api/endpoints/api.ts
@@ -0,0 +1,4 @@
+import { environment } from "src/environments/environment";
+
+export const apiBaseUrl = environment.apiUrl;
+export const imagePathPrefix = environment.imagePathPrefix;
---
+++ src/app/api/endpoints/file-upload-endpoint.ts
@@ -0,0 +1,34 @@
+import { HttpClient, HttpEventType } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { map } from "rxjs";
+import { environment } from "src/environments/environment";
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class FileUploadEndpoint {
+  baseUrl = `${environment.apiUrl}/employees/upload`;
+  constructor(private httpClient: HttpClient) { }
+
+  public imageUpload(data: FormData) {
+    return this.httpClient.post<any>(this.baseUrl, data, {
+      reportProgress: true,
+      observe: 'events'
+    }).pipe(map((event: any) => {
+
+      switch (event.type) {
+
+        case HttpEventType.UploadProgress:
+          return { status: 'progress', message: Math.round(100 * event.loaded / event.total) };
+
+        case HttpEventType.Response:
+          return event.body;
+        default:
+          return `Unhandled event: ${event.type}`;
+      }
+    })
+    );
+  }
+
+}
